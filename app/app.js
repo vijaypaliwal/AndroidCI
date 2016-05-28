c94a06b0aeb45e49e6ce33935765d426c165e09e
vijaypaliwal <vijay.paliwal@hotmail.com>
2016-05-28 14:22:06 +0530

    test
    test

--- app/app.js
+++ app/app.js
@@ -1,5 +1,5 @@
 ﻿
-var app = angular.module('AngularAuthApp', ['ngRoute', 'ngSanitize', 'LocalStorageModule', 'angular-loading-bar', 'ngCordova']);
+var app = angular.module('AngularAuthApp', ['ngRoute', 'ngSanitize', 'LocalStorageModule', 'angular-loading-bar', 'ngCordova', 'localytics.directives']);
 
 app.config(function ($routeProvider) {
 
@@ -83,10 +83,10 @@ app.config(function ($routeProvider) {
 
 });
 
-//var serviceBase = 'http://localhost:7440/API/ClearlyInventoryAPI.svc/';
-//var serviceBaseUrl = 'http://localhost:7440/';
-var serviceBaseUrl = 'http://dev.style.u8i9.com/';
-var serviceBase = 'http://dev.style.u8i9.com/API/ClearlyInventoryAPI.svc/';
+var serviceBase = 'http://localhost:7440/API/ClearlyInventoryAPI.svc/';
+var serviceBaseUrl = 'http://localhost:7440/';
+//var serviceBaseUrl = 'http://dev.style.u8i9.com/';
+//var serviceBase = 'http://dev.style.u8i9.com/API/ClearlyInventoryAPI.svc/';
 app.constant('ngAuthSettings', {
     apiServiceBaseUri: serviceBase,
     clientId: 'ngAuthApp'
