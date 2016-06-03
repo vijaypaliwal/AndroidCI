db476e2ce85419672a0df68389b0db9721bc755f
vijaypaliwal <vijay.paliwal@hotmail.com>
2016-06-03 18:53:37 +0530

    minor
    change

--- app/app.js
+++ app/app.js
@@ -1,5 +1,5 @@
 ﻿
-var app = angular.module('AngularAuthApp', ['ngRoute', 'ngSanitize', 'LocalStorageModule', 'angular-loading-bar', 'ngCordova', 'ui.sortable']);
+var app = angular.module('AngularAuthApp', ['ngRoute', 'ngSanitize', 'LocalStorageModule', 'angular-loading-bar', 'ngCordova']);
 
 app.config(function ($routeProvider) {
 
